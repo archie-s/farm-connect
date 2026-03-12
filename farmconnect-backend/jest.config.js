fefea79837195b487c29038d7394bced02daedd9
David Diah <dsdiah@gmail.com>
2026-03-12 10:29:42 +0300

    Fix failing tests and asyncHandler utility

--- farmconnect-backend/jest.config.js
+++ farmconnect-backend/jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/index.js',
-    '!src/config/**',
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  setupFilesAfterEnv: ['<rootDir>/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   testTimeout: 10000,
+  moduleNameMapper: {
+    '^../../src/app$': '<rootDir>/app',
+    '^../../src/utils/jwt$': '<rootDir>/utils/jwt',
+    '^../../src/utils/asyncHandler$': '<rootDir>/utils/asyncHandler',
+    '^../setup$': '<rootDir>/tests/setup',
+  },
 };
